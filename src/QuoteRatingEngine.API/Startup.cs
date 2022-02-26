68447d555cf4c9100408b9ebf236fc644f74a7b1
Anass Daoudi <anass1995daoudi@gmail.com>
2022-02-26 23:55:33 +0100

    Inject context using scoped type

--- src/QuoteRatingEngine.API/Startup.cs
+++ src/QuoteRatingEngine.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QuoteRatingEngine.Repository;
 
 namespace QuoteRatingEngine.API
 {
@@ -19,6 +20,7 @@ namespace QuoteRatingEngine.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<QuoteRatingEngineContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
