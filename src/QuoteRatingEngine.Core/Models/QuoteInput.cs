6f7a4e8fc491f4eaf6392c602277d3cadfe710d0
Anass Daoudi <anass1995daoudi@gmail.com>
2022-02-26 23:07:09 +0100

    Implement Equals and GetHashCode

--- src/QuoteRatingEngine.Core/Models/QuoteInput.cs
+++ src/QuoteRatingEngine.Core/Models/QuoteInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json.Serialization;
 
 namespace QuoteRatingEngine.Core.Models
@@ -13,6 +14,19 @@ namespace QuoteRatingEngine.Core.Models
         public QuoteInput()
         {
 
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is QuoteInput input &&
+                   Revenue == input.Revenue &&
+                   State == input.State &&
+                   Business == input.Business;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Revenue, State, Business);
         }
     }
 }
