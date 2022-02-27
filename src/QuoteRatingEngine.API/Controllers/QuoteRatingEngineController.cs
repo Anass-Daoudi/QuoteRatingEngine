6180061fb0ff9d6d018dbd96bc257ad7ba18eb56
Anass Daoudi <anass1995daoudi@gmail.com>
2022-02-27 02:05:35 +0100

    Inject service that communicate with Repository

--- src/QuoteRatingEngine.API/Controllers/QuoteRatingEngineController.cs
+++ src/QuoteRatingEngine.API/Controllers/QuoteRatingEngineController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using QuoteRatingEngine.API.Services;
 
 namespace QuoteRatingEngine.API.Controllers
 {
@@ -6,5 +7,11 @@ namespace QuoteRatingEngine.API.Controllers
     [ApiController]
     public class QuoteRatingEngineController : ControllerBase
     {
+        private IQuoteRatingEngineRepository _quoteRatingEngineRepository { get; }
+
+        public QuoteRatingEngineController(IQuoteRatingEngineRepository quoteRatingEngineRepository)
+        {
+            _quoteRatingEngineRepository = quoteRatingEngineRepository;
+        }
     }
 }
