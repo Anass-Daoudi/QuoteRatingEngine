dafb67833d27042584dab44dc271b9495aa37d03
Anass Daoudi <anass1995daoudi@gmail.com>
2022-02-27 02:09:19 +0100

    Add controller to compute premium

--- src/QuoteRatingEngine.API/Controllers/QuoteRatingEngineController.cs
+++ src/QuoteRatingEngine.API/Controllers/QuoteRatingEngineController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using QuoteRatingEngine.API.Services;
+using QuoteRatingEngine.Core.Models;
+using QuoteRatingEngine.Core.Services;
 
 namespace QuoteRatingEngine.API.Controllers
 {
@@ -12,6 +14,23 @@ namespace QuoteRatingEngine.API.Controllers
         public QuoteRatingEngineController(IQuoteRatingEngineRepository quoteRatingEngineRepository)
         {
             _quoteRatingEngineRepository = quoteRatingEngineRepository;
+        }
+
+        [HttpPost("ComputePremium")]
+        public ActionResult GetTotalPremium([FromBody] QuoteInput quoteInput)
+        {
+            var stateFactor = _quoteRatingEngineRepository.GetStateFactorByState(quoteInput.State);
+            var businessFactor = _quoteRatingEngineRepository.GetBusinessFactorByBusiness(quoteInput.Business);
+
+            var totalPremiumComputer = new TotalPremiumComputer(
+                stateFactor: stateFactor,
+                businessFactor: businessFactor,
+                revenue: quoteInput.Revenue
+            );
+
+            var totalPremium = totalPremiumComputer.getTotalPremium();
+
+            return Ok(new TotalPremiumResponse(totalPremium));
         }
     }
 }
